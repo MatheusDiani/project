bea6631012b9f4872fa1e21399638cb260cfec60
MatheusDiani <dianimatheus@gmail.com>
2025-02-08 00:50:17 -0300

    at

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,10 @@
-import type { Metadata } from 'next'
-import './globals.css'
-
-export const metadata: Metadata = {
-  title: 'Meu App Next.js',
-  description: 'Criado com Next.js',
-}
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="en">
       <body>{children}</body>
     </html>
   )
